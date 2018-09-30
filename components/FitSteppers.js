a736101694544cb3045ba24bc0daf122ed5c6c39
peterayeniofficial <ayenicology@gmail.com>
2018-09-30 17:29:29 +0100

    Initial commit

---
+++ components/FitSteppers.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function FitSteppers() {
+  return (
+    <View>
+      <Text>Steppers</Text>
+    </View>
+  );
+}
