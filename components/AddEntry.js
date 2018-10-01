88006fec51bc6765ef2bf1a9e670047da9e3cc93
peterayeniofficial <ayenicology@gmail.com>
2018-10-01 21:06:01 +0100

    Add Entry View

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { getMetricMetaInfo } from "../utils/helpers";
+import { View, Text, TouchableOpacity } from "react-native";
+import { getMetricMetaInfo, timeToString } from "../utils/helpers";
 import FitSteppers from "../components/FitSteppers";
 import FitSlider from "../components/FitSlider";
-import DateHeader from "../components/DateHeader"
+import DateHeader from "../components/DateHeader";
+import { Ionicons } from "@expo/vector-icons";
+import TextButton from "../components/TextButton";
+import { submitEntry, removeEntry } from "../utils/api";
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>Submit</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -43,33 +54,76 @@ export default class AddEntry extends Component {
       [metric]: value
     }));
   };
+
+  submit = () => {
+    const key = timeToString();
+    const entry = this.state;
+
+    // update Redux
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0
+    }));
+
+    // Navigate to home
+
+    submitEntry({ key, entry });
+
+    // clear local notification
+  };
+
+  reset = () => {
+    const key = timeToString();
+
+    // update Redux
+
+    // Route to Home
+
+    removeEntry(key);
+  };
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons name="ios-happy-outline" size={100} color={"black"} />
+          <Text>You already logged your information for today </Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      );
+    }
     return (
       <View>
-        <DateHeader date={(new Date()).toLocaleDateString()}/>
-        {Object.keys(metaInfo).map((key) => {
-          const { getIcon, type, ...rest } = metaInfo[key]
-          const value = this.state[key]
- 
-           return (
-             <View key={key}>
-               {getIcon()}
-               {type === 'slider'
-                 ? <FitSlider
-                     value={value}
-                     onChange={(value) => this.slide(key, value)}
-                     {...rest}
-                   />
-                 : <FitSteppers
-                     value={value}
-                     onIncrement={() => this.increment(key)}
-                     onDecrement={() => this.decrement(key)}
-                     {...rest}
-                   />}
-             </View>
+        <DateHeader date={new Date().toLocaleDateString()} />
+        {Object.keys(metaInfo).map(key => {
+          const { getIcon, type, ...rest } = metaInfo[key];
+          const value = this.state[key];
+
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === "slider" ? (
+                <FitSlider
+                  value={value}
+                  onChange={value => this.slide(key, value)}
+                  {...rest}
+                />
+              ) : (
+                <FitSteppers
+                  value={value}
+                  onIncrement={() => this.increment(key)}
+                  onDecrement={() => this.decrement(key)}
+                  {...rest}
+                />
+              )}
+            </View>
           );
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     );
   }
